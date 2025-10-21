696694a990a02b0ee01d39b0906cbb8559f03ecb
legendaaryy0 <147563011+legendaaryy0@users.noreply.github.com>
2025-10-21 05:02:58 -1100

    Describe your change

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ const toggleBtn = document.getElementById('theme-toggle');
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('light-mode');
 });
+
+const liveTime = document.getElementById('liveTime');
+
+function updateClock(){
+  const now = new Intl.DateTimeFormat('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).format();
+  liveTime.textContent = now;
+}
+updateClock();                 
+setInterval(updateClock, 1000); 
